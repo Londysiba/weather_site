1451cc37e566e6a5bb28041cbdd7f21055a4c58b
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-07-30 19:45:21 +0200

    forecast push

--- src/index.js
+++ src/index.js
@@ -22,6 +22,15 @@ function dateFormat(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
+function dayFormat(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function weatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
 
@@ -42,6 +51,47 @@ function weatherCondition(response) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
    );
   document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+
+}
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML = forecastHTML + 
+      `
+      <div class="col-2">
+      <div class="forecast-date">${dayFormat(forecastDay.dt)}</div>
+      <img
+      src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon
+      }@2x.png"
+      alt=""
+      width="42"
+      />
+      <div class="forecast-temp">
+      <span class="forecast-temp-max"> ${Math.round(forecastDay.temp.max)}° </span>
+      <span class="forecast-temp-min"> ${Math.round(forecastDay.temp.min)}° </span>
+      </div>
+      </div>
+      `;
+      }
+     });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  }
+
+function getForecast(coordinates) {
+  let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+            
+  axios.get(apiUrl).then(showForecast);
 }
 
 function search(city) {
